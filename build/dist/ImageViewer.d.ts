29ea6f605345522479539231e7b48e9ea27f68f1
christophkremsner <christopher.kremsner@loxone.com>
2023-01-24 16:57:33 +0100

    extend funtionality

---
+++ build/dist/ImageViewer.d.ts
@@ -0,0 +1,11 @@
+import React from "react";
+export type ImageViewerProps = {
+    imageUrl: string;
+    width: number;
+    height: number;
+    onRequestClose: () => unknown;
+    onSingleTap?: () => unknown;
+    sizeCallback: (translateX: number, translateY: number, scale: number, imageUrl: string) => unknown;
+};
+declare const ImageViewer: React.ForwardRefExoticComponent<ImageViewerProps & React.RefAttributes<unknown>>;
+export default ImageViewer;
